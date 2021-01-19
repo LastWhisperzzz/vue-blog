9063d67b0fd5f5fa03fb679c573996b9d6ffaaad
Last Whisper <xxzworkmail@gmail.com>
2021-01-19 19:42:00 +0800

    首页完成

---
+++ admin/src/plugins/element.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import {
+  Button,
+  Input,
+  Container,
+  Aside,
+  Menu,
+  Submenu,
+  MenuItemGroup,
+  MenuItem,
+  Header,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Main,
+  Table,
+  TableColumn
+} from 'element-ui'
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Header)
+Vue.use(Dropdown)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+Vue.use(Main)
+Vue.use(Table)
+Vue.use(TableColumn)
---
+++ admin/src/views/Layout.vue
@@ -0,0 +1,186 @@
+<template>
+  <!-- 主页 -->
+  <el-container class="layout-container">
+    <!-- 侧边栏菜单 -->
+    <el-aside :width="isCollapse ? '' : ''">
+      <el-menu
+        router
+        :default-active="$route.path"
+        background-color="#304156"
+        text-color="#BFCBD9"
+        active-text-color="#409EFF"
+        :collapse="isCollapse"
+        class="el-menu-vertical"
+      >
+        <el-menu-item index="/">
+          <i class="el-icon-odometer"></i>
+          <template slot="title">首页</template>
+        </el-menu-item>
+        <!-- 分类管理 -->
+        <el-submenu index="1">
+          <template slot="title"><i class="el-icon-menu"></i>分类管理</template>
+          <el-menu-item-group>
+            <template slot="title">分类</template>
+            <el-menu-item index="/categories">分类列表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+
+        <!-- 文章管理 -->
+        <el-submenu index="2">
+          <template slot="title"><i class="el-icon-document"></i>文章管理</template>
+          <el-menu-item-group>
+            <template slot="title">文章</template>
+            <el-menu-item index="/items">新建文章</el-menu-item>
+            <el-menu-item index="/items">文章列表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+
+        <!-- 留言管理 -->
+        <el-submenu index="3">
+          <template slot="title"><i class="el-icon-s-help"></i>留言管理</template>
+          <el-menu-item-group>
+            <template slot="title">留言</template>
+            <el-menu-item index="/items">留言列表</el-menu-item>
+            <el-menu-item index="/heroes/list">留言用户</el-menu-item>
+            <el-menu-item index="/heroes/list">文章评论</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+
+        <!-- 友链管理 -->
+        <el-submenu index="4">
+          <template slot="title"><i class="el-icon-document"></i>友链管理</template>
+          <el-menu-item-group>
+            <template slot="title">友链</template>
+            <el-menu-item index="/articles/list">友链列表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+
+        <!-- 用户管理 -->
+        <el-submenu index="6">
+          <template slot="title"><i class="el-icon-user"></i>用户管理</template>
+          <el-menu-item-group>
+            <span slot="title">用户</span>
+            <el-menu-item index="/admin_users">用户列表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <!-- 顶栏 -->
+      <el-header>
+        <!-- 折叠展开按钮 -->
+        <div>
+          <i
+            v-show="!isCollapse"
+            class="el-icon-s-unfold"
+            @click="isCollapse = true"
+            style="font-size:18px"
+          ></i>
+          <i
+            v-show="isCollapse"
+            class="el-icon-s-fold"
+            @click="isCollapse = false"
+            style="font-size:18px"
+          ></i>
+        </div>
+        <!-- 右侧菜单 -->
+        <el-dropdown class="avatar-container" trigger="click">
+          <div class="avatar-wrapper">
+            <img
+              src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80"
+              class="user-avatar"
+            />
+            <i class="el-icon-caret-bottom" />
+          </div>
+          <el-dropdown-menu slot="dropdown">
+            <a href="javascript:;">
+              <el-dropdown-item>前台页面</el-dropdown-item>
+            </a>
+            <a target="_blank" href="https://github.com/LastWhisperzzz">
+              <el-dropdown-item>项目地址</el-dropdown-item>
+            </a>
+            <el-dropdown-item divided @click.native="logout">
+              <span style="display:block;">退出登录</span>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+
+      <!-- 内容主体 -->
+      <el-main>
+        <router-view :key="$route.path"></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  data() {
+    return {
+      isCollapse: false
+    }
+  },
+  methods: {
+    logout() {
+      this.$confirm('是否退出当前账户?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        sessionStorage.clear()
+        this.$router.push('/login')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.layout-container {
+  height: 100vh;
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    .avatar-container {
+      margin-right: 30px;
+
+      .avatar-wrapper {
+        margin-top: 5px;
+        position: relative;
+
+        .user-avatar {
+          cursor: pointer;
+          width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+
+        .el-icon-caret-bottom {
+          cursor: pointer;
+          position: absolute;
+          right: -20px;
+          top: 25px;
+          font-size: 16px;
+        }
+      }
+    }
+  }
+  .el-aside {
+    background-color: #304156;
+    .el-menu {
+      border-right: none;
+    }
+  }
+  .el-main {
+    background-color: #eaedf1;
+  }
+}
+.el-menu-vertical:not(.el-menu--collapse) {
+  width: 200px;
+}
+</style>
