6841123a832fff3c1f3e71a4dced57f4778955d9
Last Whisper <xxzworkmail@gmail.com>
2021-01-25 22:44:08 +0800

    友链页完善，增加搜索

--- web/src/plugins/element.js
+++ web/src/plugins/element.js
@@ -1,6 +1,26 @@
 import Vue from 'vue'
-import { Button, Dropdown, DropdownMenu, DropdownItem } from 'element-ui'
+import {
+  Button,
+  Input,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Form,
+  FormItem,
+  Dialog,
+  Drawer,
+  Tooltip,
+  Backtop
+} from 'element-ui'
+
 Vue.use(Button)
+Vue.use(Input)
 Vue.use(Dropdown)
 Vue.use(DropdownMenu)
 Vue.use(DropdownItem)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Dialog)
+Vue.use(Drawer)
+Vue.use(Tooltip)
+Vue.use(Backtop)
