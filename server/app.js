9aa354d04aa1a50670d44806cea7ace6debed4d8
Last Whisper <xxzworkmail@gmail.com>
2021-03-08 18:10:59 +0800

    后台更新

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const history = require('connect-history-api-fallback')
 const app = express()
 
 app.set('secret', 'i2u34y12oi3u4y8')
@@ -9,8 +10,8 @@ app.use(express.json()) // 将请求解析为json
 
 // 托管静态资源
 app.use('/uploads', express.static(__dirname + '/uploads'))
-app.use('/', express.static(__dirname + '/web'))
 app.use('/admin', express.static(__dirname + '/admin'))
+app.use('/', history(), express.static(__dirname + '/web'))
 
 require('./plugins/db')(app) // 连接数据库
 
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -115,7 +115,7 @@ module.exports = app => {
   })
   app.post('/admin/api/upload', authMiddleware(), upload.single('file'), async (req, res) => {
     const file = req.file
-    file.url = `http://oss.lastwhisper.net/${file.filename}`
+    file.url = `https://oss.lastwhisper.net/${file.filename}`
     // file.url = `http://localhost:3000/uploads/${file.filename}`
     res.send(file)
   })
